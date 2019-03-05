2bf58993d7610ae27e1d85947a1bcaede218fd09
Daniel Barrett <1db2aa@gmail.com>
2019-03-05 14:42:07 -0500

    added redux to manage searchfield

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(error, info){
+        this.setState({
+            hasError: true
+        })
+    }
+
+    render() {
+        return this.state.hasError ? 
+            <h1>oops, there was an error</h1> :
+
+            this.props.children
+    }
+}
+
+export default ErrorBoundary
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css'
+
+import { setSearchField } from '../actions'
+
+const mapStatetoProps = state => {
+    return {
+        searchfield: state.searchfield
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
         }
     }
 
@@ -17,16 +34,11 @@ class App extends Component {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
-        
     }
 
-    onSearchChange = (event) => {
-        this.setState({
-            searchfield: event.target.value
-        })
-    }
     render(){
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchfield, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
@@ -35,9 +47,11 @@ class App extends Component {
             (
                 <div className="tc">
                     <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots}/>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             )
@@ -45,4 +59,4 @@ class App extends Component {
     }
 }
 
-export default App
+export default connect(mapStatetoProps, mapDispatchToProps)(App)
